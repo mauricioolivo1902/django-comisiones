79af8c52f1ffd56ac923d2a5308c76cc696136ac
Mauricio <mauricioolivo1902@gmail.com>
2025-06-25 18:48:22 -0500

    Añadir dominio de PythonAnywhere a ALLOWED_HOSTS

--- sistema_comisiones/settings.py
+++ sistema_comisiones/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j-874tgn7qp3^)cl+k!1$qhvw-^(1j%06@!q$z&$ievrylu43b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mauricioolivo.pythonanywhere.com']
 
 
 # Application definition
